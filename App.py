5d23549e56c3276f92b5c036d9738952562764f9
Benson <Benson.leehc@gmail.com>
2023-09-15 17:41:46 +0800

    Update App.py v3

--- App.py
+++ App.py
@@ -1,6 +1,11 @@
-#app.py
+"""
+https://asia-east1-linebot-mytestbot.cloudfunctions.net/App/callback
+"""
+
 from flask import Flask, request, abort
+import configparser
 import json
+
 from linebot.v3 import (
     WebhookHandler
 )
@@ -18,23 +23,25 @@ from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
+config=configparser.ConfigParser()
+config.read('config.ini')
 
 app = Flask(__name__)
 
-#line_bot_api = LineBotApi('V1PN/jDzCSzgyLhCjLrstxx0uSwiiItchaIjjM+C14GXlGCNISZ8vqBBTsTpbo5iLsQYxCehkNh34eIGHboSDu1CH/gf8cW8E2RQycLctGjdNs6XCvD7FLvWZbNLSqI8jqP5HHbOezy/osEyA6AJsAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('934b33388a3daae4f055524f2e08cb88')
-LineMessagingApi = MessagingApi('V1PN/jDzCSzgyLhCjLrstxx0uSwiiItchaIjjM+C14GXlGCNISZ8vqBBTsTpbo5iLsQYxCehkNh34eIGHboSDu1CH/gf8cW8E2RQycLctGjdNs6XCvD7FLvWZbNLSqI8jqP5HHbOezy/osEyA6AJsAdB04t89/1O/w1cDnyilFU=')
+        
+api_client = ApiClient(Configuration(access_token=config.get('line_bot', 'Channel_Token')))
+handler = WebhookHandler(config.get('line_bot', 'Channel_Secret'))
+#line_bot_api = MessagingApi(config.get('line_bot', 'Channel_Token'))
+line_bot_api = MessagingApi(api_client)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    print("callback entry")
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    #app.logger.info("Request body: " + body)
-    print(json.dumps(body, sort_keys=True,indent=4))
+    app.logger.info(json.dumps(body, sort_keys=True,indent=4))
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -42,28 +49,23 @@ def callback():
         abort(400)
 
     return 'OK'
-
-def SecretMsg():
-    print("SecretMsg received")
-    return
-##Main Message handler. add code here.
-##
-@handler.add(MessageEvent, message=TextMessage)
+"""
+Main Message handler. add code here.
+"""
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    text1 = "this is handle message"
-    #text2 = "you have message me this message : "+ event.message.text
+    app.logger.info("handle Message Entry")
+    
+    ##Corporate User only, no API permission
+    #mark_messages_as_read_request = {"chat":"userId":event.source.user_id}}
+    #line_bot_api.mark_messages_as_read(mark_messages_as_read_request)
 
-    if(event.message.text == "5356"):
-        SecretMsg()
-
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=text1)]
-            )
+    line_bot_api.reply_message_with_http_info(
+        ReplyMessageRequest(
+            reply_token=event.reply_token,
+            messages=[TextMessage(text=event.message.text)]
         )
+    )
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
